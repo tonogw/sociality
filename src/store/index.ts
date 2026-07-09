1a2655d79b6daebae95e196176eaf12b7a5b8643
tonogw <85993102+tonogw@users.noreply.github.com>
2026-07-09 14:03:13 +0700

    feat:relink import by remove aliases

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "@/store/slices/authSlice";
+import authReducer from "./slices/authSlice";
 
 export const store = configureStore({
   reducer: {
